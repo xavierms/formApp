31a002bf1807f50f96783b18115add7a4cfb1348
Xavier Mejía <xavierams08@gmail.com>
2022-01-11 21:34:15 -0400

    directive, NgForms,Forms Templates,ViewChild,etc..

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,27 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'template',
+    loadChildren: ()=> import('./template/template.module').then( x => x.TemplateModule )
+  },
+  {
+    path:'reactive',
+    loadChildren: ()=> import('./reactive/reactive.module').then( x => x.ReactiveModule )
+  },
+  {
+    path: '**',
+    redirectTo: 'template'
+  }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [
+    RouterModule.forRoot(routes)
+  ],
+  exports: [
+    RouterModule
+  ]
 })
 export class AppRoutingModule { }
---
+++ src/app/shared/sidemenu/sidemenu.component.ts
@@ -0,0 +1,45 @@
+import { Component } from '@angular/core';
+
+interface MenuItem{
+  texto: string;
+  ruta:  string;
+
+}
+
+@Component({
+  selector: 'app-sidemenu',
+  templateUrl: './sidemenu.component.html'
+})
+export class SidemenuComponent  {
+
+ templateMenu: MenuItem[]=[
+   {
+     texto: 'Básicos',
+     ruta:  './template/basicos'
+   },
+   {
+    texto: 'Dinámicos',
+    ruta:  './template/dinamicos'
+   },
+   {
+     texto: 'Switch',
+     ruta:  './template/switches'
+   }
+ ]
+
+ reactiveMenu: MenuItem[]=[
+  {
+    texto: 'Básicos',
+    ruta:  './reactive/basicos'
+  },
+  {
+   texto: 'Dinámicos',
+   ruta:  './reactive/dinamicos'
+  },
+  {
+    texto: 'Switch',
+    ruta:  './reactive/switches'
+  }
+]
+
+}
