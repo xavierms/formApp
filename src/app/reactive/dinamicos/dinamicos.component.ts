245c9e5c76501905fa3d3c06b365e20fb9fde5e1
Xavier Mejía <xavierams08@gmail.com>
2022-01-16 20:04:31 -0400

    reactive forms dinamics

--- src/app/reactive/dinamicos/dinamicos.component.ts
+++ src/app/reactive/dinamicos/dinamicos.component.ts
@@ -1,45 +1,66 @@
 import { Component, OnInit } from '@angular/core';
-import { FormArray, FormBuilder, FormGroup, Validators, Validator, FormControl } from '@angular/forms';
+import {
+  FormArray,
+  FormBuilder,
+  FormGroup,
+  Validators,
+  Validator,
+  FormControl,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-dinamicos',
   templateUrl: './dinamicos.component.html',
-  styles: [
-  ]
+  styles: [],
 })
 export class DinamicosComponent implements OnInit {
-
-  constructor(private formBuilder: FormBuilder) { }
+  constructor(private formBuilder: FormBuilder) {}
 
   miFormulario: FormGroup = this.formBuilder.group({
-    nombre:[,[Validators.required,Validators.minLength(3)]],
-    favoritos: this.formBuilder.array( [
-     ['meta', Validators.required],
-     ['death',Validators.required],
-    ],Validators.required )
+    nombre: ['', [Validators.required, Validators.minLength(3)]],
+    favoritos: this.formBuilder.array(
+      [
+        ['meta', Validators.required],
+        ['death', Validators.required],
+      ],
+      Validators.required)
   });
 
-  nuevoFavorito: FormControl = this.formBuilder.control('',Validators.required);
-  get favoritosArr(){
+  nuevoFavorito: FormControl = this.formBuilder.control(
+    '',
+    Validators.required
+  );
+  get favoritosArr() {
     return this.miFormulario.get('favoritos') as FormArray;
   }
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
-  validarForm(field: string){
-    return  this.miFormulario.controls[field].errors
-                           && this.miFormulario.controls[field].touched
-    }
-guardar(){
-  if (this.miFormulario.invalid  ) {
-    this.miFormulario.markAllAsTouched();
-    return ;
-    
+  validarForm(field: string) {
+    return (
+      this.miFormulario.controls[field].errors &&
+      this.miFormulario.controls[field].touched
+    );
   }
   
-  console.log('save');
-  console.log(this.miFormulario.value);
-  this.miFormulario.reset();
-  
-}
+ addFavorito(){
+   if (this.nuevoFavorito.invalid) { return; }
+
+  //  this.favoritosArr.push( new FormControl(this.nuevoFavorito.value, Validators.required) );
+   this.favoritosArr.push(this.formBuilder.control( this.nuevoFavorito.value, Validators.required));
+
+   this.nuevoFavorito.reset();
+ }
+ borrar(index: number){
+this.favoritosArr.removeAt(index)
+ }
+  guardar() {
+    if (this.miFormulario.invalid) {
+      this.miFormulario.markAllAsTouched();
+      return;
+    }
+
+    console.log('save');
+    console.log(this.miFormulario.value);
+    this.miFormulario.reset();
+  }
 }
