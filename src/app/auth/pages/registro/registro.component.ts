206c987c53f32e6daee30840fdfce32134740bb6
Xavier Mejía <xavierams08@gmail.com>
2022-01-17 22:48:34 -0400

    email validation,custom validations

--- src/app/auth/pages/registro/registro.component.ts
+++ src/app/auth/pages/registro/registro.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-registro',
@@ -11,13 +11,33 @@ export class RegistroComponent implements OnInit {
   
   constructor(private fb: FormBuilder) { }
   
-  ngOnInit(): void {}
+
+  noEsStrider(control: FormControl){
+     const valor = control.value?.trim().toLowerCase();
+     if (valor === 'strider') {
+       return {
+         noStrider: true
+       }
+     }
+     return null;
+     
+     
+  }
+  ngOnInit(): void {
+    this.miFormulario.reset({
+      nombre: 'Xavier Mejia',
+      email: 'xms@mail.com',
+      
+    })
+  }
 
   nombreApellidoPattern: string = '([a-zA-ZÀ-ÿ]+) ([a-zA-ZÀ-ÿ]+)';
+  emailPattern: string = '^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$';
   
   miFormulario: FormGroup = this.fb.group({
     nombre: ['', [Validators.required, Validators.pattern(this.nombreApellidoPattern) ]],
-    email : ['', [Validators.required, Validators.pattern(this.nombreApellidoPattern) ]]
+    email : ['', [Validators.required, Validators.pattern(this.emailPattern) ]],
+    username : ['', [Validators.required, this.noEsStrider ]]
   })
 
   campoNoValido(campo: string){
